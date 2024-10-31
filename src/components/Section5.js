89b770fd7c02e68a2cfb3efca28c71bb0fa6377b
leeyoungjik1 <eog911@gmail.com>
2024-10-31 15:50:41 +0900

    section5 문구 수정

--- src/components/Section5.js
+++ src/components/Section5.js
@@ -1,12 +1,4 @@
-import { useState } from "react";
-
 const Section5 = () => {
-    const [isModalOpen, setModalOpen] = useState(false)
-
-    const handleToggleModal = () => {
-        setModalOpen(prevState => !prevState)
-    }
-
     return (
         <section className='section5'>
             <div className='sectionWarp'>
@@ -15,65 +7,67 @@ const Section5 = () => {
                 </div>
                 <div className="content1">
                     <div className="bgc2">Speaking Video</div>
-                    <div>
-                        <div className="bgc1" onClick={handleToggleModal}>Check Korean, Native Language Script ➔</div>
+                    <div className="modal">
+                        <div className="modalContent">
+                            <div className="textBox">
+                                <p>
+                                    <span className="span1">
+                                        안녕하세요! 저는 HDX 북미법인 글로벌스탭 찰스입니다. 우리 법인은 2010년에 만들어졌어요.<br></br>
+                                        그리고 우리 법인은 미국 로스앤젤레스에 위치해 있어요.
+                                    </span><br></br>
+                                    <span className="span2">(Hello! I'm Charles, the global staff of HDX North America. Our branch was created in 2010. 
+                                        And our branch is located in Los Angeles, USA.)</span><br></br>
+                                    <span className="span3">L.A. is a city full of culture and art. With its stunning beaches and spectacular Hollywood, 
+                                    it's full of life. </span><br></br><br></br>
 
-                        {isModalOpen && (
-                            <div className="modal">
-                                <div className="modalContent">
-                                    <div className="textBox">
-                                        <p>
-                                            <span>저는 HDX 북미법인 글로벌스탭 찰스입니다.</span><br></br>
-                                            (Hello! I'm Charles, the global staff of HDX North America)<br></br><br></br>
+                                    <span className="span1">
+                                    이 곳에서 5명의 주재원과 40명의 글로벌스탭이 함께 근무해요.
+                                    </span><br></br>
+                                    <span className="span2">(Five expatriates and 40 global staff work together here.)</span><br></br>
+                                    <span className="span3">Everyone is kind and fun to work with.</span><br></br><br></br>
 
-                                            <span>우리 법인은 2010년에 만들어졌어요.</span> <br></br>
-                                            (Our branch was created in 2010)<br></br><br></br>
+                                    <span className="span1">
+                                    우리 법인은 건설기계를 판매해요.
+                                    </span><br></br>
+                                    <span className="span2">(we sells construction machinery.)</span><br></br>
+                                    <span className="span3">Our excavator is the number one in this city. Isn't it cool?</span><br></br><br></br>
 
-                                            <span>그리고 우리 법인은 미국 로스앤젤레스에 위치해 있어요.</span> <br></br>
-                                            (And our branch is located in Los Angeles, USA.)<br></br><br></br>
+                                    <span className="span1">
+                                    우리 법인장님은 김현대 상무님이에요. 
+                                    </span><br></br>
+                                    <span className="span2">(our president is Hyundai kim)</span><br></br>
+                                    <span className="span3">He is a master of excavators. 법인장님 know everything about excavators. <br></br>
+                                    He also introduced me to Korean food, so I tried it and it was really delicious. <br></br>
+                                    My favorite menu is 불고기!<br></br><br></br>
+                                    
+                                    We have 10 people on our team, and we promote excavators. <br></br>
+                                    This is our best colleague, Michael.
+                                    </span><br></br><br></br>
 
-                                            L.A. is a city full of culture and art.<br></br>
-                                            With its stunning beaches and spectacular Hollywood, it's full of life.<br></br><br></br>
+                                    <span className="span1">
+                                    우리는 산책을 좋아해요.
+                                    </span><br></br>
+                                    <span className="span2">(We like to take a walk)</span><br></br>
+                                    <span className="span3">We have lunch together and enjoy a short walk around the neighborhood.</span><br></br><br></br>
 
-                                            <span>이 곳에서 5명의 주재원과 40명의 글로벌스탭이 함께 근무해요.</span><br></br>
-                                            (Five expatriates and 40 global staff work together here.)<br></br><br></br>
+                                    <span className="span1">
+                                    우리는 해변에서 피크닉을 하고 싶어요.
+                                    </span><br></br>
+                                    <span className="span2">(We want to have a picnic on the beach.)</span><br></br>
+                                    <span className="span3">Recently, we discussed with our team the idea of having a picnic on the beach. <br></br>
+                                    We want to enjoy delicious food while looking at the sea and listening to each other's stories.<br></br><br></br>
+                                    
+                                    How was the introduction of our HDX American branch ?<br></br>
+                                    Please keep supporting us. 
+                                    </span><br></br><br></br>
 
-                                            Everyone is kind and fun to work with.<br></br><br></br>
-
-                                            <span>우리 법인은 건설기계를 판매해요.</span> (we sells construction machinery.)<br></br><br></br>
-
-                                            Our excavator is the number one in this city. Isn't it cool?<br></br><br></br>
-
-                                            <span>우리 법인장님은 김현대 상무님이에요.</span> (our president is Hyundai kim)<br></br><br></br>
-
-                                            He is a master of excavators. 법인장님 know everything about excavators.<br></br>
-                                            He also introduced me to Korean food, so I tried it and it was really delicious. <br></br>
-                                            My favorite menu is 불고기!<br></br><br></br>
-
-                                            We have 10 people on our team, and we promote excavators.<br></br>
-                                            This is our best colleague, Michael. <br></br><br></br>
-
-                                            <span>우리는 산책을 좋아해요.</span> (We like to take a walk)<br></br>
-                                            We have lunch together and enjoy a short walk around the neighborhood.<br></br><br></br>
-
-                                            <span>우리는 해변에서 피크닉을 하고 싶어요.</span><br></br>
-                                            (We want to have a picnic on the beach.)<br></br><br></br>
-
-                                            Recently, we discussed with our team the idea of having a picnic on the beach.<br></br>
-                                            We want to enjoy delicious food while looking at the sea and listening to each other's stories.<br></br><br></br>
-
-                                            How was the introduction of our HDX American branch ?<br></br>
-                                            Please keep supporting us.<br></br><br></br>
-
-                                            <span>감사합니다! 사랑해요 HD현대</span><br></br>
-                                            (thank you I love HDhyundai)
-                                        </p>
-                                    </div>
-                                    <div className="close bgc3" onClick={handleToggleModal}>Close</div>
-                                </div>
+                                    <span className="span1">
+                                    감사합니다! 사랑해요 HD현대
+                                    </span><br></br>
+                                    <span className="span2">(thank you I love HD hyundai)</span><br></br>
+                                </p>
                             </div>
-                        )}
-
+                        </div>
                     </div>
                 </div>
                 <div className="content2">
--- src/components/chinese/Section5.js
+++ src/components/chinese/Section5.js
@@ -1,11 +1,4 @@
-import { useState } from "react";
-
 const Section5 = () => {
-    const [isModalOpen, setModalOpen] = useState(false)
-
-    const handleToggleModal = () => {
-        setModalOpen(prevState => !prevState)
-    }
 
     return (
         <section className='section5'>
@@ -15,30 +8,62 @@ const Section5 = () => {
                 </div>
                 <div className="content1">
                     <div className="bgc2">演讲视频</div>
-                    <div className="bgc1" onClick={handleToggleModal}>确认韩语指南 ➔</div>
-                    {isModalOpen && (
-                        <div className="modal">
-                            <div className="modalContent">
-                                <div className="textBox">
-                                    <p>
-                                        <span>1. 00법인은 0000년도에 설립되었어요/만들어졌어요.</span><br></br>
-                                        = 00法人在0000年成立。<br></br><br></br>
-                                        <span>2. 00법인은 00(나라명) 00(도시)에 위치해 있어요.<br></br>
-                                        이 곳에는 00명의 주재원과 00명의 글로벌스탭이 함께 근무를 해요.</span><br></br>
-                                        = 00法人位于00（国家名）00（城市）。<br></br>
-                                        这里有00名外派员工和00名全球员工一起工作。<br></br><br></br>
-                                        <span>3. 우리 00법인은 00 제품을 만들고/판매하고 있어요.</span><br></br>
-                                        = 我们00法人生产/销售00产品。<br></br><br></br>
-                                        <span>4. 우리는 00을 좋아해요</span><br></br>
-                                        = 我们喜欢00。/ 我们的00喜欢做00。<br></br><br></br>
-                                        <span>5. 우리는 00를 하고 싶어요.</span><br></br>
-                                        = 我们想要做00。/ 我们的00想要00。
-                                    </p>
-                                </div>
-                                <div className="close bgc3" onClick={handleToggleModal}>关闭</div>
+                    <div className="modal">
+                        <div className="modalContent">
+                            <div className="textBox">
+                                <p>
+                                    <span className="span1">
+                                        안녕하세요! 저는 HDX 북미법인 글로벌스탭 찰스입니다. 우리 법인은 2010년에 만들어졌어요.<br></br>
+                                        그리고 우리 법인은 미국 로스앤젤레스에 위치해 있어요.
+                                    </span><br></br>
+                                    <span className="span2">(大家好！我是HDX北美法人全球团队的查尔斯。<br></br>
+                                        我们的法人在2010年成立。我们的法人位于美国洛杉矶。)</span><br></br>
+                                    <span className="span3">洛杉矶是一个充满文化和艺术的城市。这里有美丽的海滩和华丽的好莱坞，充满活力。</span><br></br><br></br>
+
+                                    <span className="span1">
+                                    이 곳에서 5명의 주재원과 40명의 글로벌스탭이 함께 근무해요.
+                                    </span><br></br>
+                                    <span className="span2">(这里有5名外派员工和40名全球员工一起工作。)</span><br></br>
+                                    <span className="span3">大家都很友善，一起愉快地工作。</span><br></br><br></br>
+
+                                    <span className="span1">
+                                    우리 법인은 건설기계를 판매해요.
+                                    </span><br></br>
+                                    <span className="span2">(我们的法人销售建筑机械。)</span><br></br>
+                                    <span className="span3">在这个城市，我们的挖掘机是第一名。很棒吧？</span><br></br><br></br>
+
+                                    <span className="span1">
+                                    우리 법인장님은 김현대 상무님이에요. 
+                                    </span><br></br>
+                                    <span className="span2">(我们的法人负责人是金现代。)</span><br></br>
+                                    <span className="span3">他是挖掘机的专家，对挖掘机了如指掌。<br></br>
+                                    法人负责人向我介绍了韩国料理，我尝了一下，真的很好吃。我最喜欢的菜是烤肉。<br></br>
+                                    我们团队有10个人，负责挖掘机的推广工作。这位是我们团队里最好的朋友迈克尔。
+                                    </span><br></br><br></br>
+
+                                    <span className="span1">
+                                    우리는 산책을 좋아해요.
+                                    </span><br></br>
+                                    <span className="span2">(我们喜欢散步。)</span><br></br>
+                                    <span className="span3">午餐后，我们会一起在附近走走，享受短暂的休息。</span><br></br><br></br>
+
+                                    <span className="span1">
+                                    우리는 해변에서 피크닉을 하고 싶어요.
+                                    </span><br></br>
+                                    <span className="span3">我们想在海滩上野餐。<br></br>
+                                    最近我们讨论了想和团队成员一起在海滩上野餐。<br></br>
+                                    想一边看海，一边享用美味的食物，听听彼此的故事，度过愉快的时光。<br></br><br></br>
+                                    我们北美法人的介绍怎么样？希望大家继续关注我们。 
+                                    </span><br></br><br></br>
+
+                                    <span className="span1">
+                                    감사합니다! 사랑해요 HD현대
+                                    </span><br></br>
+                                    <span className="span3">谢谢！我爱HD现代！</span><br></br>
+                                </p>
                             </div>
                         </div>
-                    )}
+                    </div>
                 </div>
                 <div className="content2">
                     <div className="bgc2">描述视频</div>
